bd76eac73a16979377e19087359f4d760e6fb986
SimretN <134206815+SimretN@users.noreply.github.com>
2024-01-28 21:25:52 +0100

    create table in init.sql

--- .devcontainer/init-sql/init.sql
+++ .devcontainer/init-sql/init.sql
@@ -1,1 +1,30 @@
 -- Lägg till din exporterade SQL kod här
+
+
+
+
+SET NAMES utf8;
+SET time_zone = '+00:00';
+SET foreign_key_checks = 0;
+SET sql_mode = 'NO_AUTO_VALUE_ON_ZERO';
+
+DROP TABLE IF EXISTS `todos`;
+CREATE TABLE `todos` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `title` text NOT NULL,
+  `taskDescription` text NOT NULL,
+  `date_time` datetime NOT NULL DEFAULT current_timestamp(),
+  `checked` tinyint(1) NOT NULL DEFAULT 0,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+INSERT INTO `todos` (`id`, `title`, `taskDescription`, `date_time`, `checked`) VALUES
+(52,	'HTML',	'Hypertext Markup Language',	'2024-01-27 20:35:26',	0),
+(53,	'CSS',	'Cascading Style sheets',	'2024-01-27 20:36:21',	0),
+(54,	'JavaScript',	'DOM manipulation',	'2024-01-27 20:37:25',	0),
+(55,	'PHP',	'Hypertext Preprocessor',	'2024-01-27 20:38:32',	0),
+(56,	'Laravel',	'Artisan Console',	'2024-01-27 20:39:28',	1);
+
+-- 2024-01-28 20:15:51
+
+
